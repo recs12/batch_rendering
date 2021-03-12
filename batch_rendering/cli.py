a6cbd6451f3f5bde95db40b50a4ca41b06b6ca9b
slimane rechdi <slimane.rechdi@gmail.com>
2021-03-12 14:05:07 -0500

    add features compting and timer

--- batch_rendering/cli.py
+++ batch_rendering/cli.py
@@ -1,11 +1,14 @@
 import subprocess
+import time
 
 import click
 import pandas as pd
+import datetime
 
 
 def render_one_item(commands: list, mode: str):
     if mode == "SE":
+        print(f"[INFO] SE - Part Number: {commands[2]}")
         mode, source, source_file, folder_target, file_target = (
             commands[0],
             commands[1],
@@ -17,8 +20,11 @@ def render_one_item(commands: list, mode: str):
             f"SEToolRender -m {mode} -f {source} -i {source_file} -o {folder_target} -q {file_target}",
             shell=True,
             check=True,
+            capture_output=True,
+            text=True,
         )
     elif mode == "TC":
+        print(f"[INFO] TC - Part Number: {commands[1]}")
         mode, item_id, revision, user, password, group, role, server, folder_target = (
             commands[0],
             commands[1],
@@ -34,12 +40,15 @@ def render_one_item(commands: list, mode: str):
             f"SEToolRender -m {mode} -i {item_id} -f {revision} -u {user} -p {password} -g {group} -r {role} -s {server} -o {folder_target}",
             shell=True,
             check=True,
+            capture_output=True,
+            text=True,
         )
     else:
         raise NotImplementedError
 
+
 @click.command()
-@click.argument('excel')
+@click.argument("excel")
 def batch_rendering(excel):
     """Run SEToolRendering in batch with an excel file."""
     df = pd.read_excel(
@@ -47,18 +56,37 @@ def batch_rendering(excel):
         index_col=None,
         dtype={
             "mode (-m)": str,
+            "folder target (-o)": str,
             "folder source (-f)": str,
             "file source (-i)": str,
-            "folder target (-o)": str,
             "file target (-q)": str,
+            "item ID (-i)": str,
             "revision (-f)": str,
+            "user (-u)": str,
+            "password (-p)": str,
+            "group (-g)": str,
+            "role (-r)": str,
+            "server (-s)": str,
         },
     )
 
     # Define the shape of the excel file.
     number_rows, number_columns = df.shape
 
-    # For each row run a command line.
-    for row in range(number_rows):
-        commands: list = [df.iat[row, column] for column in range(number_columns)]
-        render_one_item(commands, mode=commands[0])
+    try:
+        tic = time.perf_counter()
+        # For each row run a command line.
+        for row in range(number_rows):
+            commands: list = [df.iat[row, column] for column in range(number_columns)]
+            render_one_item(commands, mode=commands[0])
+            print(f"[OK  ] iteration: [{row+1}/{number_rows}]")
+
+    except subprocess.CalledProcessError as c:
+        with open("log_rendering_errors.txt", "a") as err:
+            err.write(f"[FAIL] Excel line: {row+1}")
+            print(f"[FAIL] iteration: [{row+1}/{number_rows}]")
+
+    finally:
+        toc = time.perf_counter()
+        seconds = toc - tic
+        print(f"Time to completion: {datetime.timedelta(seconds=seconds)}")
