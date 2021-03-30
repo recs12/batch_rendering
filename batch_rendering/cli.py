87360227909215c9665d7ba27911d57e456613ef
slimane rechdi <slimane.rechdi@gmail.com>
2021-03-30 10:24:09 -0400

    add tempo when disconected

--- batch_rendering/cli.py
+++ batch_rendering/cli.py
@@ -2,16 +2,30 @@
 Only works in mode TC.
 """
 
+import datetime
+import getpass
+import os
+import pathlib
 import subprocess
 import time
-import os
 
 import click
 import pandas as pd
-import datetime
 from loguru import logger
-import pathlib
 
+# import notifiers
+
+# params = {
+#     "username": "slimane.rechdi@gmail.com",
+#     "password": "abc123", # prompt with click
+#     "to": "dest@gmail.com"
+# }
+
+# Send a single notification
+# notifier = notifiers.get_notifier("gmail")
+# notifier.notify(message="The application is running!", **params)
+
+user_name = getpass.getuser()
 
 def render_one_item(items, user, password, folder_target, group, role, server, mode):
     print(f"[INFO] TC - Part Number: {items[1]}")
@@ -21,12 +35,15 @@ def render_one_item(items, user, password, folder_target, group, role, server, m
         f"SEToolRender {mode} -i {item_id} -v {revision} -u {user} -p {password} -g {group} -r {role} -s {server} -o {folder_target}",
         shell=True,
         check=False,
-        capture_output=False,
-        text=True, # display the main macro output
-        stderr= subprocess.DEVNULL, # ignore the errors 
+        capture_output=True,
+        text=True,  # display the main macro output
+        stderr=subprocess.DEVNULL,  # ignore the errors
     )
+
+    # Logging of the error message.
     if p1.returncode != 0:
         logger.error(f"[ERROR] : {p1.stderr}")
+
     # check the presence of the files (json, bmp)
     bmp = os.path.join(folder_target, f"{item_id}-Rev-{revision}.bmp")
     json = os.path.join(folder_target, f"{item_id}-Rev-{revision}.json")
@@ -37,22 +54,16 @@ def render_one_item(items, user, password, folder_target, group, role, server, m
     else:
         logger.error(f"[FAIL ] : {item_id} Files not downloaded")
 
+    # If server disconnection code 666 then tempo of 15 min.
+    if p1 == 666:
+        time.sleep(60*15)
+
 
 # maybe we could add some defaults values.
-@click.command(
-    help="excel.xlsx -u <user> -p <password> -o <target-folder> <group>* <role>* <server>*"
-)
+@click.command(help="Ctrl + C to stop the macro.")
 @click.argument("excel", nargs=1, required=True, type=click.Path(exists=True))
-@click.option("--user", "-u", nargs=1, required=True, type=str, help="Your acronym.")
-@click.option(
-    "--password",
-    "-p",
-    prompt=True,
-    confirmation_prompt=True,
-    hide_input=True,
-    required=True,
-    help="Your password.",
-)
+@click.option("--user", "-u", nargs=1, required=False, type=str, help="Your acronym.")
+@click.password_option()
 @click.option(
     "--folder_target",
     "-o",
@@ -64,59 +75,76 @@ def render_one_item(items, user, password, folder_target, group, role, server, m
 @click.option(
     "--group",
     "-g",
-    default="Engineering", show_default=True,
+    default="Engineering",
+    show_default=True,
     nargs=1,
     required=False,
-    type=click.Choice(['Engineering']),
+    type=click.Choice(["Engineering"]),
     help="Group optional.",
 )
 @click.option(
     "--role",
     "-r",
-    default="Designer", show_default=True,
+    default="Designer",
+    show_default=True,
     nargs=1,
     required=False,
-    type=click.Choice(['Designer']),
+    type=click.Choice(["Designer"]),
     help="Role optional.",
 )
 @click.option(
     "--server",
     "-s",
-    default="TC_PROD", show_default=True,
+    default="TC_PROD",
+    show_default=True,
     nargs=1,
     required=False,
-    type=click.Choice(['TC_PROD']),
+    type=click.Choice(["TC_PROD"]),
     help="Server optional.",
 )
 @click.option(
     "--mode",
     "-m",
-    default="TC", show_default=True,
+    default="TC",
+    show_default=True,
     nargs=1,
     required=False,
-    type=click.Choice(['TC']),
+    type=click.Choice(["TC"]),
     help="TC is the only mode enabled.",
+)
+@click.option(
+    "--debug-mode",
+    default="ERROR",
+    required=False,
+    type=click.Choice(["ERROR", "INFO"]),
+    help="Debug mode for logging file.",
 )
 def batch_rendering(
     excel,
-    user,
     password,
     folder_target,
     group,
     role,
     server,
     mode,
+    debug_mode,
+    user=user_name,
 ):
     """Run SEToolRendering in batch with an excel file."""
 
     logger.add(
-        "report_rendering.log",
+        "rendering_error.log",
         format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}",
-        level="INFO",
+        level=debug_mode,
     )
-
+    print(f"Logging mode: {debug_mode}")
     df = pd.read_excel(
-        excel, index_col=None, dtype={"Jde": str, "Revision": str} #names of columns in excel must be excatly the same.
+        excel,
+        index_col=None,
+        dtype={
+            "ID": str,
+            "Revision": str,
+        },  # names of columns in excel must be excatly the same.
     )
     # Define the shape of the excel file.
     number_rows, number_columns = df.shape
@@ -126,12 +154,13 @@ def batch_rendering(
         # For each row run a command line.
         for row in range(number_rows):
             items: list = [df.iat[row, column] for column in range(number_columns)]
-            render_one_item(items, user, password, folder_target, group, role, server, mode)
+            render_one_item(
+                items, user, password, folder_target, group, role, server, mode
+            )
             logger.info(f"[ITERATION] {row+1}/{number_rows}")
 
     except subprocess.CalledProcessError:
         logger.exception({row + 1})
-
 
     finally:
         toc = time.perf_counter()
